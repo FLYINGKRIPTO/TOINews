2cdfd71ed642b785fa95f39f99b8f2193846f61a
FLYINGKRIPTO <srivastava.shivanshu4@gmail.com>
2018-09-27 11:22:21 +0530

    Written NewsFeatures.java code, contains data about the contents of the app

---
+++ app/src/main/java/com/example/dell/toinews/NewsFeatures.java
@@ -0,0 +1,51 @@
+package com.example.dell.toinews;
+
+import android.graphics.Bitmap;
+
+import java.net.URL;
+
+public class NewsFeatures {
+    private String author;
+    private String title;
+    private String description;
+    private URL articleUrl;
+    private Bitmap thumbnail;
+    private String dateTime;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public URL getArticleUrl() {
+        return articleUrl;
+    }
+
+    public Bitmap getThumbnail() {
+        return thumbnail;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public NewsFeatures(String author, String title, String description, URL articleUrl,
+                        Bitmap thumbnail, String dateTime){
+      this.author = author;
+      this.title = title;
+      this.description = description;
+      this.articleUrl = articleUrl;
+      this.thumbnail = thumbnail;
+      this.dateTime = dateTime;
+
+  }
+
+
+}
