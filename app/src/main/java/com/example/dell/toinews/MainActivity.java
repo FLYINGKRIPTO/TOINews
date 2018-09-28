1b49af3f8e66091e5430d71b4d47269b040f19d7
FLYINGKRIPTO <srivastava.shivanshu4@gmail.com>
2018-09-28 06:25:32 +0530

    Written code for Loader in mainActivity

--- app/src/main/java/com/example/dell/toinews/MainActivity.java
+++ app/src/main/java/com/example/dell/toinews/MainActivity.java
@@ -6,7 +6,9 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<NewsFeatures>> {
@@ -17,7 +19,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ListView newsListView = findViewById(R.id.list);
         ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
         if(networkInfo==null||!networkInfo.isConnected()){
@@ -28,22 +30,26 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             LoaderManager loaderManager = getLoaderManager();
             loaderManager.initLoader(NEWS_LOADER_ID,null,this);
         }
-        
-
+      newsAdapter = new NewsAdapter(this,new ArrayList<NewsFeatures>());
+      newsListView.setAdapter(newsAdapter);
     }
 
     @Override
     public Loader<List<NewsFeatures>> onCreateLoader(int id, Bundle args) {
-        return null;
+        return new NewsLoader(this,REQUESTED_URL);
     }
 
     @Override
     public void onLoadFinished(Loader<List<NewsFeatures>> loader, List<NewsFeatures> data) {
-
+         if(data!=null&&!data.isEmpty()){
+             newsAdapter.addAll(data);
+         }
+         else{
+             newsAdapter.clear();
+             }
     }
-
     @Override
     public void onLoaderReset(Loader<List<NewsFeatures>> loader) {
-
+        newsAdapter.clear();
     }
 }
